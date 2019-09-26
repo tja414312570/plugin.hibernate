f9da91a927e64615912f5970b37e0f2d19108e42
tja414312570 <414312570@qq.com>
2019-09-26 11:00:55 +0800

    新增抽象事物，支持事物隔离级别，事物传播行为，支持注解事物

---
+++ src/com/YaNan/frame/hibernate/database/transaction/exception/TransactionRequiredException.java
@@ -0,0 +1,13 @@
+package com.YaNan.frame.hibernate.database.transaction.exception;
+
+public class TransactionRequiredException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5977314470044190726L;
+	public TransactionRequiredException() {
+		super("");
+	}
+
+}
