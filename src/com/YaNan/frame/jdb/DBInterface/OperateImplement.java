3c9b17683e18680d5f3b5aea1d25431c0313046d
tja414312570 <414312570@qq.com>
2019-10-16 10:08:18 +0800

    Update OperateImplement.java

--- src/com/YaNan/frame/jdb/DBInterface/OperateImplement.java
+++ src/com/YaNan/frame/jdb/DBInterface/OperateImplement.java
@@ -3,9 +3,6 @@ package com.YaNan.frame.jdb.DBInterface;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.YaNan.frame.jdb.DataTable;
 
 public abstract class OperateImplement {
